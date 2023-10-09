f227f243609ae0753b3c1d04eca5732c07201f30
Souparno Ghosh <76548636+SouparnoGhosh@users.noreply.github.com>
2023-10-09 21:08:06 +0530

    Basic scaffolding (#1)
    * Basic scaffolding
    
    * Changes to status string

---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+	// Define a handler function that writes "Hello, World" to the response.
+	handler := func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprint(w, "Hello World!")
+	}
+
+	// Register the handler function for the root ("/") route.
+	http.HandleFunc("/", handler)
+
+	// Start the web server on port 8080.
+	fmt.Println("Server is running on http://localhost:8080")
+	http.ListenAndServe(":8080", nil)
+}
